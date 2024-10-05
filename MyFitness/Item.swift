fd2cce961e7aea2a2dfe722db722a3b38070497d
simonemayers <simone21295@gmail.com>
2024-10-05 15:45:27 -0400

    Initial Commit

---
+++ MyFitness/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MyFitness
+//
+//  Created by Owner on 10/5/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
